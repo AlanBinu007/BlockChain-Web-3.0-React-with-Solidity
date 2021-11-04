0040eb018e9902f0adb9dbb7c0c9ed2b290a13a3
Alan Binu <65335042+AlanBinu007@users.noreply.github.com>
2021-11-04 21:14:33 +0530

    Added heart loagin screen

--- src/components/Loader/LoaderLogo.js
+++ src/components/Loader/LoaderLogo.js
@@ -1,57 +1,35 @@
 import React from "react";
 import "./LoaderLogo.css";
-import "./LoaderLogo.scss";
 
 class LogoLoader extends React.Component {
   render() {
+    document.addEventListener("mousemove", function (e) {
+      var body = document.querySelector("body");
+      var heart = document.createElement("span1");
+      var x = e.offsetX;
+      var y = e.offsetY;
+      heart.style.left = x + "px";
+      heart.style.top = y + "px";
+      var size = Math.random() * 100;
+      heart.style.width = 20 + size + "px";
+      heart.style.height = 20 + size + "px";
+      // if(window.location.href=="https://alanbinu-portfolio.web.app/#/" || window.location.href=="https://alanbinu-portfolio.web.app/#/splash"){
+      //   body.appendChild(heart);
+      // }
+      if(window.location.href=="http://localhost:3000/#/" || window.location.href=="http://localhost:3000/#/splash"){
+        body.appendChild(heart);
+      }
+      setTimeout(function () {
+        heart.remove();
+      }, 2000);
+    });
+
     const theme = this.props.theme;
-
     return (
-      <>
-        <br />
-        <main>
-        
-          <div class="dank-ass-loader">
-
-            <div class="row">
-              <div class="arrow up outer outer-18"></div>
-              <div class="arrow down outer outer-17"></div>
-              <div class="arrow up outer outer-16"></div>
-              <div class="arrow down outer outer-15"></div>
-              <div class="arrow up outer outer-14"></div>
-            </div>
-            <div class="row">
-              <div class="arrow up outer outer-1"></div>
-              <div class="arrow down outer outer-2"></div>
-              <div class="arrow up inner inner-6"></div>
-              <div class="arrow down inner inner-5"></div>
-              <div class="arrow up inner inner-4"></div>
-              <div class="arrow down outer outer-13"></div>
-              <div class="arrow up outer outer-12"></div>
-            </div>
-            <div class="row">
-              <div class="arrow down outer outer-3"></div>
-              <div class="arrow up outer outer-4"></div>
-              <div class="arrow down inner inner-1"></div>
-              <div class="arrow up inner inner-2"></div>
-              <div class="arrow down inner inner-3"></div>
-              <div class="arrow up outer outer-11"></div>
-              <div class="arrow down outer outer-10"></div>
-            </div>
-            <div class="row">
-              <div class="arrow down outer outer-5"></div>
-              <div class="arrow up outer outer-6"></div>
-              <div class="arrow down outer outer-7"></div>
-              <div class="arrow up outer outer-8"></div>
-              <div class="arrow down outer outer-9"></div>
-            </div>
-          </div>
-          <br />
-          <h4 class="animate">Loading</h4>
-          
-        </main>
-        <div id="text"><span>No Matter How Hard it is ,</span> Just Keep Going</div>
-      </>
+    <>
+    <div id="text"><span>No Matter How Hard it is ,</span> Just Keep Going</div>
+    <div class="loader"></div>
+    </>
     );
   }
 }
